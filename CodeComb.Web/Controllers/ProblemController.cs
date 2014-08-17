8480ac59c5f1ffcae02ffaf658e3c6ab6beadd78
Gasai Yuno <1@4321.io>
2014-08-17 14:52:11 +0800

    problem page finished

--- CodeComb.Web/Controllers/ProblemController.cs
+++ CodeComb.Web/Controllers/ProblemController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace CodeComb.Web.Controllers
 {
-    public class ProblemController : BaseController
+    public class ProblemController : ContestBaseController
     {
         //
         // GET: /Problem/
@@ -17,6 +17,10 @@ namespace CodeComb.Web.Controllers
         public ActionResult Show(int id)
         {
             var problem = DbContext.Problems.Find(id);
+            var contest = problem.Contest;
+            var user = ViewBag.CurrentUser == null ? new Entity.User() : (Entity.User)ViewBag.CurrentUser;
+            if (DateTime.Now < contest.Begin && user.Role < Entity.UserRole.Master && (from m in contest.Managers select m.ID).ToList().Contains(user.ID))
+                return RedirectToAction("Message", "Shared", new { msg = "您无权查看该题目！" });
             var Problems = new List<Models.View.StatusSnapshot>();
             foreach (var p in problem.Contest.Problems.OrderBy(x=>x.Credit))
                 Problems.Add(new Models.View.StatusSnapshot(p.Contest, ViewBag.CurrentUser));
