9994a4f4083ffbb61191cd1b083c224173f1b708
Gasai Yuno <1@4321.io>
2014-08-21 10:13:38 +0800

    ckeditor config

--- CodeComb.Web/Scripts/ckeditor/config.js
+++ CodeComb.Web/Scripts/ckeditor/config.js
@@ -11,12 +11,16 @@ CKEDITOR.editorConfig = function( config ) {
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	
 	config.toolbarGroups = [
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
 		{ name: 'links' },
-		{ name: 'insert'},
+		{ name: 'insert' },
 		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
 		{ name: 'others' },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		'/',
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align'] },
 		{ name: 'styles' },
 		{ name: 'colors' },
 	];
