92b7f738723512240947896bd798ec1903c402cb
Gasai Yuno <1@4321.io>
2014-08-26 17:29:32 +0800

    castmsg

--- CodeComb.Web/Scripts/codecomb.basic.js
+++ CodeComb.Web/Scripts/codecomb.basic.js
@@ -59,3 +59,14 @@
         }
     });
 })(window);
+
+function CastMsg(msg) {
+    $(document.body).append("<divt class='show_public_tips'><p class='show_public_tips_text'>" + msg + "</p></div>");
+    $(".show_public_tips").fadeIn();
+    setTimeout(function () {
+        $(".show_public_tips").fadeOut();
+        setTimeout(function () {
+            $(".show_public_tips").remove();
+        }, 1000);
+    }, 2000);
+}
